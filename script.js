fa8ebde5380c72d6f851a3c692bcd33260e23f22
AadityaMunjal <aadityamunjal06@gmail.com>
2020-12-19 23:05:16 +0530

    A lot of stuff was done while git was not there

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.onload = () => {
+//   const display = document.getElementById("display");
+//   const fireCrackerContainer = document.getElementById("fireworks");
+
+//   const firecrackerDivClass = "firecracker";
+
+//   display.addEventListener("click", addFireCracker);
+
+//   const addFireCracker = () => {
+//     const firecracker = document.createElement("div");
+//     firecracker.className = firecrackerDivClass;
+//     fireCrackerContainer.appendChild(firecracker);
+//   };
+// };
